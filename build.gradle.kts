ba14872292bde792e27b56eb4938f75028d572b9
kostoev05 <kostoev.05@mail.ru>
2022-12-11 21:32:31 +0300

    Changed version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             artifactId = "state-dsl"
             groupId = "io.github.evkost"
-            version = "0.1.0-beta02"
+            version = "1.0.0-beta02"
 
             from(components["java"])
 
